f1cd8d5b0d09c02f313f93921db533d7d5335750
chenwei <chenwei@qq.com>
2019-04-23 21:41:05 +0800

    通用枚举添加：结算日枚举类、结算方式枚举类

---
+++ pay-common-client/src/main/java/com/pay/common/client/constants/CheckDayEnum.java
@@ -0,0 +1,33 @@
+package com.pay.common.client.constants;
+
+/**
+ * 结算日枚举
+ *
+ * @author chenwei
+ * @date 2019-04-23
+ */
+public enum CheckDayEnum {
+
+    /**
+     * 自然日+0
+     */
+    D0,
+
+    /**
+     * 自然日+1
+     */
+    D1,
+
+    /**
+     * 交易日+0
+     */
+    T0,
+
+    /**
+     * 交易日+1
+     */
+    T1,
+
+    ;
+
+}
---
+++ pay-common-client/src/main/java/com/pay/common/client/constants/CheckMethodEnum.java
@@ -0,0 +1,22 @@
+package com.pay.common.client.constants;
+
+/**
+ * 结算方式
+ *
+ * @author chenwei
+ * @date 2019-04-23
+ */
+public enum CheckMethodEnum {
+
+    /**
+     * 自动
+     */
+    AUTO,
+
+    /**
+     * 手动
+     */
+    MANUAL,
+
+    ;
+}
