8ef56c3c726c3b10c3605005e1c42f77dd1a8bc8
chenwei <chenwei@qq.com>
2019-05-12 10:36:24 +0800

    结算状态枚举类

---
+++ pay-common-client/src/main/java/com/pay/common/client/constants/CheckStatusEnum.java
@@ -0,0 +1,17 @@
+package com.pay.common.client.constants;
+
+/**
+ * @author chenwei
+ * @date 2019-05-04
+ */
+public enum CheckStatusEnum {
+    /**
+     * 待结算
+     */
+    WAIT,
+
+    /**
+     * 已结算
+     */
+    CHECKED,
+}
